7714d8525ce6535cf0ac1223b0439f86d632a96b
Justintime50 <jhammond@easypost.com>
2020-07-01 17:56:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WithBond EasyPost Carrier Integration
+
+The carrier integration for WithBond on the EasyPost platform.
---
+++ src/curl.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+curl --location --request POST https://public-api.int01.withbond.io/api/v1/orders \
+    --header 'X-BOND-KEY: 123...' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{
+        "type": "DELIVER",
+        "receiver": {
+            "name": "John Smith",
+            "email": "mr.smith@example.com",
+            "phone": "+1-917-685-3957",
+            "zipcode": "10003-1502",
+            "address": "228 Park Ave S",
+            "city": "New York",
+            "state": "NY",
+            "country": "USA"
+        },
+        "packages": [
+        {
+            "items": [
+                {
+                    "sku": "12",
+                    "quantity": 2
+                }
+            ],
+            "dimensions": {
+                "unit": "IMPERIAL",
+                "height": 1,
+                "width": 6,
+                "length": 9,
+                "weight": 2.25
+            },
+            "specialCondition": "Extremly heavy, handle with care"
+        }
+    ]
+    }'
