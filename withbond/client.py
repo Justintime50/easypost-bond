2eaaf735b272ae4af0739ad42916ca897cac1727
Bradley Walsh <bradleywalsh@Bradleys-iMac.local>
2020-07-08 16:49:58 -0700

    changed some function names, added a test script test.py to test the API calls, and got the basic functionality of the Shipment.create function built.

--- withbond/client.py
+++ withbond/client.py
@@ -1,14 +1,24 @@
 """This file builds the API request and houses shared constants"""
+import os
+from dotenv import load_dotenv
 import requests
 
+load_dotenv()
+API_KEY = os.getenv('WITHBOND_API_KEY')
+headers = {
+            'X-BOND-KEY': API_KEY,
+            'Content-Type': 'application/json'
+          }
 
 class Client():
     """The client class builds the API request and is used throughout the library"""
     VERSION = '1.0.0'
-    API_BASE_URL = "https://public-api.int01.withbond.io/api/v1/"
+    API_BASE_URL = "https://public-api.int01.withbond.io/api/v1"
+    
 
     @classmethod
-    def response(cls, data, endpoint):
+    def request(cls, httpMethod, endpoint, data):
+        
         """Build the API request and return it to the method invoking it"""
-        request = requests.post(endpoint, data=data)
-        return request
+        response = requests.request(httpMethod, endpoint, headers=headers, data=data)
+        return response
